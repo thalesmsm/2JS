030225541301abd722b6a2133f51df3c8c126e73
Thales Maciel <98064558+thalesmsm@users.noreply.github.com>
2022-02-04 10:26:46 -0300

    ex015

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -1,26 +1,55 @@
-
-
 function verificar() {
-    var agora = new Date()
-    var anoAtual = agora.getFullYear()
-    var idade = anoAtual - Number(document.querySelector('#txtano').value)
-    var img = document.querySelector('#imagem')
-    var selecaoMulher = document.querySelector('#feminino')
+    var data = new Date()
+    var anoAtual = data.getFullYear()
+    var fAno = document.getElementById('txtano') //formulario que pega o ano de nascimento
     
-    if (selecaoMulher == 'Feminino') {
-        if (idade >= 60) {
-            img.src = './imagens/foto-idoso-f.png'
-            document.body.style.backgroundColor = 'pink'
-        } else if (idade >= 30) {
-            img.src = './imagens/foto-adulto-f.png'
-        } else if (idade >= 15) {
-            img.src = './imagens/foto-jovem-f.png'
-        } else {
-            img.src = './imagens/foto-bebe-f.png'
-            
-        }
+    var res = document.querySelector('#res')
+    if (fAno.value.length == 0 || fAno.value > anoAtual) { //verificar se o input de ano de nascimento esta vazio ou ano maior que atual
+        window.alert('Digite o ano de nascimento válido')
     } else {
-        img.src ='./imagens/foto-idoso-m.png'
+        var fsex = document.getElementsByName('radsex')
+        var idade = anoAtual - Number(fAno.value)
+        var genero = ''
+        var img = document.createElement('img') // para colocar foto dinamicamente pelo JS devemos criar um elemento a partir do document.createElement('img') o img será a 'tag'
+        img.setAttribute('id', 'foto') // daí cria-se um atributo onde é dado um id foto para a tag img criada 
+        // essas ultimas 2 linhas de código representam <img id="foto> escrito em HTML"
+        if (fsex[0].checked) {
+            genero = 'homem'
+            document.body.style.backgroundColor = 'blue'
+            if (idade < 10) {
+                //crianca
+                img.src = './imagens/foto-bebe-m.png'
+            } else if (idade < 23) {
+                // jovem
+                img.setAttribute('src', './imagens/foto-jovem-m.png')
+            }else if (idade < 50) {
+                //adulto
+                img.src = './imagens/foto-adulto-m.png'
+            } else {
+                //idoso
+                img.setAttribute('src', './imagens/foto-idoso-m.png')
+            }
+        } else if (fsex[1].checked) {
+            genero = 'mulher'
+            document.body.style.backgroundColor = 'pink'
+            if (idade < 10) {
+                //crianca
+                img.setAttribute('src', './imagens/foto-bebe-f.png')
+            } else if (idade < 23) {
+                // jovem
+                img.setAttribute('src', './imagens/foto-jovem-f.png')
+            }else if (idade < 50) {
+                //adulto
+                img.setAttribute('src', './imagens/foto-adulto-f.png')
+            } else {
+                //idoso
+                img.setAttribute('src', './imagens/foto-idoso-f.png')
+            }
+        }
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos`
+        res.appendChild(img) // (imagino que)como não tem uma tag e imagem no conteúdo da pag vindo do HTML, usa o apppendChild para adicionar a imagem ao document através do JS. não apenas trocando o conteudo atraves do JS
+        
     }
+             
 }
 
