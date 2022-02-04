c8598b8c2e9f7aa689fa8d1b6ed2b887aa9d0fa9
Thales Maciel <98064558+thalesmsm@users.noreply.github.com>
2022-02-04 13:22:00 -0300

    estrutura de repetição while e do while

---
+++ aula13/rep(do)while.js
@@ -0,0 +1,12 @@
+/* var n = 0                estrutura de repetição com teste lógico no começo
+ var i = 1
+ while (i < 4) {
+     console.log(i)
+     i++
+ } 
+ */
+var i = 1           // estrutura de repetição com teste lógico no final
+do {
+    console.log(i)
+    i++
+} while (i <= 5)
